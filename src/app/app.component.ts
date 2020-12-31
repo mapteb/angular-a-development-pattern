1dbb21892bb5af1d908e205f15517515bef98736
Nalla Senthilnathan <nrsenthil@gmail.com>
2020-12-31 10:56:54 -0500

    docs folder revised
    docs folder revised

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,6 @@ export class AppComponent implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit(): void {
-    this.router.navigate(["https://mapteb.github.io/angular-a-development-pattern"]);
+    this.router.navigate(["/"]);
   }
 }
