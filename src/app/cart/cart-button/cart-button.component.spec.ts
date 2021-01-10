a4d9af24322b4fde2b0b25dc3ea62cc4c93c1e48
Nalla Senthilnathan <nrsenthil@gmail.com>
2021-01-10 16:24:45 -0500

    fixed cart-button.component.spec.ts
    fixed cart-button.component.spec.ts

--- src/app/cart/cart-button/cart-button.component.spec.ts
+++ src/app/cart/cart-button/cart-button.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 import { CartButtonComponent } from './cart-button.component';
 
 describe('CartButtonComponent', () => {
@@ -8,6 +9,10 @@ describe('CartButtonComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        AppRoutingModule,
+        RouterTestingModule.withRoutes([]),
+    ],
       declarations: [ CartButtonComponent ]
     })
     .compileComponents();
